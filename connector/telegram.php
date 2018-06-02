0a902bba66fe94d7c89d421badb566d95851a455
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-03 00:51:48 +0700

    Turn off dummy webhook

--- connector/telegram.php
+++ connector/telegram.php
@@ -1,65 +1,65 @@
 <?php
 
-$argv[1] = '{
-    "update_id": 344827803,
-    "message": {
-        "message_id": 58901,
-        "from": {
-            "id": 243692601,
-            "is_bot": false,
-            "first_name": "Ammar",
-            "last_name": "F.",
-            "username": "ammarfaizi2",
-            "language_code": "en-US"
-        },
-        "chat": {
-            "id": 243692601,
-            "first_name": "Ammar",
-            "last_name": "F.",
-            "username": "ammarfaizi2",
-            "type": "private"
-        },
-        "date": 1527954473,
-        "reply_to_message": {
-            "message_id": 58900,
-            "from": {
-                "id": 243692601,
-                "is_bot": false,
-                "first_name": "Ammar",
-                "last_name": "F.",
-                "username": "ammarfaizi2",
-                "language_code": "en-US"
-            },
-            "chat": {
-                "id": 243692601,
-                "first_name": "Ammar",
-                "last_name": "F.",
-                "username": "ammarfaizi2",
-                "type": "private"
-            },
-            "date": 1527954468,
-            "forward_from": {
-                "id": 198245216,
-                "is_bot": false,
-                "first_name": "Septian",
-                "last_name": "Hari",
-                "username": "liqrgv",
-                "language_code": "en-us"
-            },
-            "forward_date": 1527952001,
-            "text": "Kayak samsung?"
-        },
-        "text": "zxczxc",
-        "entities": [
-            {
-                "offset": 0,
-                "length": 6,
-                "type": "bot_command"
-            }
-        ]
-    }
-}';
-$argv[1] = rawurlencode($argv[1]);
+// $argv[1] = '{
+//     "update_id": 344827803,
+//     "message": {
+//         "message_id": 58901,
+//         "from": {
+//             "id": 243692601,
+//             "is_bot": false,
+//             "first_name": "Ammar",
+//             "last_name": "F.",
+//             "username": "ammarfaizi2",
+//             "language_code": "en-US"
+//         },
+//         "chat": {
+//             "id": 243692601,
+//             "first_name": "Ammar",
+//             "last_name": "F.",
+//             "username": "ammarfaizi2",
+//             "type": "private"
+//         },
+//         "date": 1527954473,
+//         "reply_to_message": {
+//             "message_id": 58900,
+//             "from": {
+//                 "id": 243692601,
+//                 "is_bot": false,
+//                 "first_name": "Ammar",
+//                 "last_name": "F.",
+//                 "username": "ammarfaizi2",
+//                 "language_code": "en-US"
+//             },
+//             "chat": {
+//                 "id": 243692601,
+//                 "first_name": "Ammar",
+//                 "last_name": "F.",
+//                 "username": "ammarfaizi2",
+//                 "type": "private"
+//             },
+//             "date": 1527954468,
+//             "forward_from": {
+//                 "id": 198245216,
+//                 "is_bot": false,
+//                 "first_name": "Septian",
+//                 "last_name": "Hari",
+//                 "username": "liqrgv",
+//                 "language_code": "en-us"
+//             },
+//             "forward_date": 1527952001,
+//             "text": "Kayak samsung?"
+//         },
+//         "text": "zxczxc",
+//         "entities": [
+//             {
+//                 "offset": 0,
+//                 "length": 6,
+//                 "type": "bot_command"
+//             }
+//         ]
+//     }
+// }';
+// $argv[1] = rawurlencode($argv[1]);
 
 if (isset($argv[1])) {
 	require __DIR__."/../vendor/autoload.php";
