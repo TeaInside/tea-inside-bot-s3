49206bcf1ae8e98f0b952f7d2cc254f154ca7f5b
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-02 23:18:23 +0700

    Update config example

--- connector/telegram.php
+++ connector/telegram.php
@@ -1,35 +1,36 @@
 <?php
 
-// $argv[1] = '{
-//     "update_id": 344806834,
-//     "message": {
-//         "message_id": 57401,
-//         "from": {
-//             "id": 243692601,
-//             "is_bot": false,
-//             "first_name": "Ammar",
-//             "last_name": "F.",
-//             "username": "ammarfaizi2",
-//             "language_code": "en-US"
-//         },
-//         "chat": {
-//             "id": 243692601,
-//             "first_name": "Ammar",
-//             "last_name": "F.",
-//             "username": "ammarfaizi2",
-//             "type": "private"
-//         },
-//         "date": 1525455505,
-//         "text": "/sh echo 123123",
-//         "entities": [
-//             {
-//                 "offset": 0,
-//                 "length": 6,
-//                 "type": "bot_command"
-//             }
-//         ]
-//     }
-// }';
+$argv[1] = '{
+    "update_id": 69827616,
+    "message": {
+        "message_id": 6381,
+        "from": {
+            "id": 243692601,
+            "is_bot": false,
+            "first_name": "Ammar",
+            "last_name": "F.",
+            "username": "ammarfaizi2",
+            "language_code": "en-US"
+        },
+        "chat": {
+            "id": 243692601,
+            "first_name": "Ammar",
+            "last_name": "F.",
+            "username": "ammarfaizi2",
+            "type": "private"
+        },
+        "date": 1527956107,
+        "text": "/sh echo 123",
+        "entities": [
+            {
+                "offset": 0,
+                "length": 3,
+                "type": "bot_command"
+            }
+        ]
+    }
+}';
+$argv[1] = rawurlencode($argv[1]);
 
 if (isset($argv[1])) {
 	require __DIR__."/../vendor/autoload.php";
