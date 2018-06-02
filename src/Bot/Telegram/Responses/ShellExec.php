7e7db281899ae8f2ad5e180bdcd5a2241319a9b9
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-03 01:34:09 +0700

    Update shell exec

--- src/Bot/Telegram/Responses/ShellExec.php
+++ src/Bot/Telegram/Responses/ShellExec.php
@@ -29,7 +29,7 @@ class ShellExec extends ResponseFoundation
 			fflush($fp);
 			fclose($fp);
 			shell_exec("sudo chmod +x ".$filename);
-			$shell = shell_exec("cd /home/ammarfaizi2 && sudo -u ammarfaizi2 ".$filename." 2>&1");
+			$shell = shell_exec("cd /home/ammarfaizi2 && ".$filename." 2>&1");
 			shell_exec("sudo rm -f ".$filename);
 			$reply = "<pre>".htmlspecialchars($shell, ENT_QUOTES, "UTF-8")."</pre>";
 			Exe::sendMessage(
