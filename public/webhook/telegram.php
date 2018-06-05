2bb7fdcb4a9c233206adf4141f83824b7801f789
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-05 21:53:31 +0700

    Update telegram session

--- public/webhook/telegram.php
+++ public/webhook/telegram.php
@@ -2,12 +2,9 @@
 
 require __DIR__."/../../config/telegram/log_dir";
 
-$in = file_get_contents("php://input");
-
 shell_exec(
 	"nohup ".
-	"/usr/bin/php".
-	" ".
+	"/usr/bin/php ".
 	__DIR__."/../../connector/telegram.php \"".
-	rawurlencode($in)."\" >> \"{$logDir}/bg.log\" 2>&1 &"
+	rawurlencode(file_get_contents("php://input"))."\" >> \"{$logDir}/bg.log\" 2>&1 &"
 );
