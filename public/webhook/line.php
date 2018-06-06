71e04efb8cf84b3f35f794a4a43ce15c855098f4
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-06 17:09:25 +0700

    Prepare line webhook and debug

--- public/webhook/line.php
+++ public/webhook/line.php
@@ -5,6 +5,6 @@ require __DIR__."/../../config/line/log_dir";
 shell_exec(
 	"nohup ".
 	"/usr/bin/php ".
-	__DIR__."/../../connector/telegram.php \"".
+	__DIR__."/../../connector/line.php \"".
 	rawurlencode(file_get_contents("php://input"))."\" >> \"{$logDir}/bg.log\" 2>&1 &"
 );
