335b24b1591c278929f16ba6642877c65457cad0
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-06 23:59:09 +0700

    Turn off debugging

--- connector/line.php
+++ connector/line.php
@@ -1,24 +1,24 @@
 <?php
 
 
-$argv[1] = '{
-    "events": [
-        {
-            "type": "message",
-            "replyToken": "06a98eb295944419914a93106f764c9d",
-            "source": {
-                "groupId": "Ce20228a1f1f98e6cf9d6f6338603e962",
-                "userId": "U547ba62dc793c6557abbb42ab347f15f",
-                "type": "group"
-            },
-            "timestamp": 1528302841417,
-            "message": {
-                "type": "image",
-                "id": "8076192883200"
-            }
-        }
-    ]
-}';
+// $argv[1] = '{
+//     "events": [
+//         {
+//             "type": "message",
+//             "replyToken": "06a98eb295944419914a93106f764c9d",
+//             "source": {
+//                 "groupId": "Ce20228a1f1f98e6cf9d6f6338603e962",
+//                 "userId": "U547ba62dc793c6557abbb42ab347f15f",
+//                 "type": "group"
+//             },
+//             "timestamp": 1528302841417,
+//             "message": {
+//                 "type": "image",
+//                 "id": "8076192883200"
+//             }
+//         }
+//     ]
+// }';
 
 if (isset($argv[1])) {
     require __DIR__."/../vendor/autoload.php";
