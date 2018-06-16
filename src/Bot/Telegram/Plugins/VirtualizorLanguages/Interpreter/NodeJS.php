59510831618ebb4ec5d36770a338518231a4dfc3
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-16 22:02:07 +0700

    Update NodeJS memory limit

--- src/Bot/Telegram/Plugins/VirtualizorLanguages/Interpreter/NodeJS.php
+++ src/Bot/Telegram/Plugins/VirtualizorLanguages/Interpreter/NodeJS.php
@@ -54,7 +54,7 @@ class NodeJS extends Interpreter
 			file_put_contents($f, $this->code);
 		}
 
-		$st->setMemoryLimit(1024 * 256);
+		$st->setMemoryLimit(1024 * 1000);
 		$st->setMaxProcesses(5);
 		$st->setMaxWallTime(10);
 		$st->setMaxExecutionTime(5);
