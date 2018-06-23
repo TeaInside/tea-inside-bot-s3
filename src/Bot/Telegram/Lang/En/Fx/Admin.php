074c60fe07345544995ea0692ba8b82fb1b38e0f
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-23 21:18:18 +0700

    Fix kick lang

--- src/Bot/Telegram/Lang/En/Fx/Admin.php
+++ src/Bot/Telegram/Lang/En/Fx/Admin.php
@@ -6,7 +6,7 @@ final class Admin
 {
 	public static $map = [
 		"banned_success" => ":admin banned :banned_user!",
-		"kicked_success" => ":admin banned :kicked_user!",
+		"kicked_success" => ":admin kicked :kicked_user!",
 		"promote_success" => ":new_admin has been promoted to be an administrator by :promotor!",
 		"need_reply" => "You need to reply a message to use this command!",
 		"sudo_only" => "You must to be a sudoer to promote user!",
