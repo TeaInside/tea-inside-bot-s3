6c1e18c87fcec7a3f552b485efc74aeaac42cb6f
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-23 21:01:20 +0700

    Fix lang

--- src/Bot/Telegram/Lang/En/SubMap.php
+++ src/Bot/Telegram/Lang/En/SubMap.php
@@ -7,6 +7,7 @@ final class SubMap
 	public static $subMap = [
 		"welcome.set_success" => "Welcome",
 		"admin.banned_success" => "Admin",
-		"admin.promote_success" => "Admin"
+		"admin.promote_success" => "Admin",
+		"admin.command_not_allowed" => "Admin"
 	];
 }
