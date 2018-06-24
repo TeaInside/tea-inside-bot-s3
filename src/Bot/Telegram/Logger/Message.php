5d8d0843e923f00d6b9a799ad823dfef2fd98364
Ammar Faizi <ammarfaizi2@gmail.com>
2018-06-24 11:06:50 +0700

    Update message logger

--- src/Bot/Telegram/Logger/Message.php
+++ src/Bot/Telegram/Logger/Message.php
@@ -62,38 +62,6 @@ class Message implements LoggerInterface
 			]
 		);
 		$lastInsertId = $this->pdo->lastInsertId();
-		if ($this->data["msg_type"] === "text") {
-			$st = $this->pdo->prepare("INSERT INTO `private_messages_data` (`message_id`, `text`, `file`, `type`) VALUES (:message_id, :text, :file, :type);");
-			$st->execute(
-				[
-					":message_id" => $lastInsertId,
-					":text" => $this->data["text"],
-					":file" => null,
-					":type" => "text"
-				]
-			);
-		}
-	}
-
-	/**
-	 * @return void
-	 */
-	private function saveGroupMessage()
-	{
-		$st = $this->pdo->prepare("INSERT INTO `group_messages` (`group_id`, `tmsg_id`, `user_id`, `reply_to_tmsg_id`, `created_at`) VALUES (:group_id, :tmsg_id, :user_id, :reply_to_tmsg_id, :created_at);");
-		$st->execute(
-			[
-				":group_id" => $this->data["group_id"],
-				":tmsg_id" => $this->data["msg_id"],
-				":user_id" => $this->data["user_id"],
-				":reply_to_tmsg_id" => (isset($this->data->in["message"]["reply_to_message"]) ? $this->data->in["message"]["reply_to_message"]["message_id"] : null),
-				":created_at" => date("Y-m-d H:i:s")
-			]
-		);
-		$lastInsertId = $this->pdo->lastInsertId();
-
-		$st = $this->pdo->prepare("INSERT INTO `group_messages_data` (`message_id`, `text`, `file`, `type`) VALUES (:message_id, :text, :file, :type);");
-		var_dump($this->data["msg_type"]);
 		switch ($this->data["msg_type"]) {
 			case "text":
 				$st->execute(
@@ -116,7 +84,66 @@ class Message implements LoggerInterface
 				);
 				break;
 			default:
-				# code...
+				$st->execute(
+					[
+						":message_id" => $lastInsertId,
+						":text" => $this->data["text"],
+						":file" => null,
+						":type" => "unknown"
+					]
+				);
+				break;
+		}
+	}
+
+	/**
+	 * @return void
+	 */
+	private function saveGroupMessage()
+	{
+		$st = $this->pdo->prepare("INSERT INTO `group_messages` (`group_id`, `tmsg_id`, `user_id`, `reply_to_tmsg_id`, `created_at`) VALUES (:group_id, :tmsg_id, :user_id, :reply_to_tmsg_id, :created_at);");
+		$st->execute(
+			[
+				":group_id" => $this->data["group_id"],
+				":tmsg_id" => $this->data["msg_id"],
+				":user_id" => $this->data["user_id"],
+				":reply_to_tmsg_id" => (isset($this->data->in["message"]["reply_to_message"]) ? $this->data->in["message"]["reply_to_message"]["message_id"] : null),
+				":created_at" => date("Y-m-d H:i:s")
+			]
+		);
+		$lastInsertId = $this->pdo->lastInsertId();
+
+		$st = $this->pdo->prepare("INSERT INTO `group_messages_data` (`message_id`, `text`, `file`, `type`) VALUES (:message_id, :text, :file, :type);");
+		switch ($this->data["msg_type"]) {
+			case "text":
+				$st->execute(
+					[
+						":message_id" => $lastInsertId,
+						":text" => $this->data["text"],
+						":file" => null,
+						":type" => $this->data["msg_type"]
+					]
+				);
+				break;
+			case "photo":
+				$st->execute(
+					[
+						":message_id" => $lastInsertId,
+						":text" => $this->data["text"],
+						":file" => $this->data['photo'][count($this->data['photo']) - 1]["file_id"],
+						":type" => $this->data["msg_type"]
+					]
+				);
+				break;
+			default:
+				$st->execute(
+					[
+						":message_id" => $lastInsertId,
+						":text" => $this->data["text"],
+						":file" => null,
+						":type" => "unknown"
+					]
+				);
 				break;
 		}
 	}
