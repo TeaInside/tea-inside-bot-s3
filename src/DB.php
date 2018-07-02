1214f2e7f431ffba98423b80909edfa7f0a4fd16
Ammar Faizi <ammarfaizi2@gmail.com>
2018-07-02 14:30:22 +0700

    Add some PDO attribute

--- src/DB.php
+++ src/DB.php
@@ -27,8 +27,13 @@ final class DB
 		$this->pdo = new PDO(
 			"mysql:host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME,
 			DB_USER,
-			DB_PASS
+			DB_PASS,
+			[
+				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_PERSISTENT => true
+			]
 		);
+		
 	}
 
 	/**
