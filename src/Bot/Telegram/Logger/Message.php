da1a3e560a6e5ce62d8a43bc7a381bf514662f0d
Ammar Faizi <ammarfaizi2@gmail.com>
2018-07-02 14:26:10 +0700

    Prepare upgrade logger

--- src/Bot/Telegram/Logger/Message.php
+++ src/Bot/Telegram/Logger/Message.php
@@ -3,6 +3,7 @@
 namespace Bot\Telegram\Logger;
 
 use DB;
+use Bot\Telegram\Exe;
 use Bot\Telegram\Data;
 use Bot\Telegram\Contracts\LoggerInterface;
 
@@ -48,6 +49,54 @@ class Message implements LoggerInterface
 	}
 
 	/**
+	 * @return int
+	 */
+	private function savePhoto()
+	{
+		$p = $this->data['photo'][count($this->data['photo']) - 1];
+		$fileid = $p['file_id'];
+		unset($p["file_id"]);
+	    $a = json_decode(Exe::getFile([
+	        "file_id" => 
+	    ])["out"], true);
+	    if (isset($a["result"]["file_path"])) {
+	        $ch = curl_init("https://api.telegram.org/file/bot".TOKEN."/".$a['result']['file_path']);
+	        curl_setopt_array($ch,
+	        	[
+	        		CURLOPT_RETURNTRANSFER => true,
+	        		CURLOPT_SSL_VERIFYPEER => false,
+	        		CURLOPT_SSL_VERIFYHOST => false
+	        	]
+	        );
+	        $binary = curl_exec($ch);
+	        $filename = ($sha1 = sha1($binary))."_".($md5 = md5($binary)).".jpg";
+	        $handle = fopen(file_storage."/".$filename, "w");
+	        fwrite($handle, $binary);
+	        fclose($handle);
+	        $now = date("Y-m-d H:i:s");
+	        $this->pdo->prepare(
+	        	"INSERT INTO `files` 
+	        	(`type`, `md5_checksum`, `sha1_checksum`, `filesize`, `hit_count`, `telegram_file_id`, `info`, `created_at`, `last_hit`) 
+	        	VALUES
+	        	(:type, :md5_checksum, :sha1_checksum, :filesize, :hit_count, :telegram_file_id, :info, :created_at, :last_hit);"
+	        )->execute(
+	        	[
+	        		":tyoe" => "photo",
+	        		":md5_checksum" => $md5,
+	        		":sha1_checksum" => $sha1_checksum,
+	        		":filesize" => strlen($binary),
+	        		":hit_count" => 1,
+	        		":telegram_file_id" => $p["file_id"],
+	        		":info" => json_encode($p),
+	        		":created_at" => $now,
+	        		":last_hit" => $now
+	        	]
+	        );
+	        return $this->pdo->lastInsertId();
+	    }
+	}
+
+	/**
 	 * @return void
 	 */
 	private function savePrivateMessage()
@@ -63,26 +112,20 @@ class Message implements LoggerInterface
 		);
 		$lastInsertId = $this->pdo->lastInsertId();
 		$st = $this->pdo->prepare("INSERT INTO `private_messages_data` (`message_id`, `text`, `file`, `type`) VALUES (:message_id, :text, :file, :type);");
+		$data =	[
+			":message_id" => $lastInsertId,
+			":text" => $this->data["text"],
+			":file" => null,
+			":type" => $this->data["msg_type"]
+		];
 		switch ($this->data["msg_type"]) {
 			case "text":
-				$st->execute(
-					[
-						":message_id" => $lastInsertId,
-						":text" => $this->data["text"],
-						":file" => null,
-						":type" => $this->data["msg_type"]
-					]
-				);
+				$st->execute($data);
 				break;
 			case "photo":
-				$st->execute(
-					[
-						":message_id" => $lastInsertId,
-						":text" => $this->data["text"],
-						":file" => $this->data['photo'][count($this->data['photo']) - 1]["file_id"],
-						":type" => $this->data["msg_type"]
-					]
-				);
+				$fx = $this->savePhoto();
+				$data[":file"] = $fx;
+				$st->execute($data);
 				break;
 			case "sticker":
 				$file_id = $this->data["sticker"]["file_id"];
