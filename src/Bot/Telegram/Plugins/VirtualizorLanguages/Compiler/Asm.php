bed96714f3467f21af554c0b00550ea95788e17b
Ammar Faizi <ammarfaizi2@gmail.com>
2018-08-05 13:46:44 +0700

    Update Asm handler

--- src/Bot/Telegram/Plugins/VirtualizorLanguages/Compiler/Asm.php
+++ src/Bot/Telegram/Plugins/VirtualizorLanguages/Compiler/Asm.php
@@ -58,7 +58,7 @@ class Asm extends Compiler
 	{
 		$id = $this->uniqueId = Isolator::generateUserId($userId);
 		$this->isolator = new Isolator($id);
-
+		$f = ISOLATOR_HOME."/".$id."/u".$id."/".($n = $this->generateFilename()).".asm";
 		file_put_contents($f, $this->code);
 		$exe = trim(shell_exec("sudo nasm -f elf ".$f." -o ".ISOLATOR_HOME."/".$id."/u".$id."/".$n.".o 2>&1"));
 		var_dump("exe1: ".$exe);
